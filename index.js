3307c99f14faf04a88b655025f6fff8e54869601
Ahmed Vally <142688910+Ahmed-Vally@users.noreply.github.com>
2024-03-29 05:22:11 +0200

    did some more styling in css. styled the button the colors and the placement of words. Addded the new fonts in html. linked my code to firebase in js, Addded some more code to make the app more refined.

--- index.js
+++ index.js
@@ -2,12 +2,53 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-663e3-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    
+    clearShoppingListEl()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+      
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+        
+        appendItemToShoppingListEl (currentItemValue)
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
